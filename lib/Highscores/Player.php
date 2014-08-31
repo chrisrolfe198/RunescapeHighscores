c12ffcfe5e1b1e32a1789a57befe851dcbecb8ec
Christopher Rolfe <christopher.rolfe198@gmail.com>
2014-08-31 13:10:17 +0100

    Attempting to migrate out skills and minigames

--- lib/Highscores/Player.php
+++ lib/Highscores/Player.php
@@ -7,9 +7,10 @@ use \OutOfRangeException;
 
 class Player implements PlayerInterface
 {
-	protected $attrs = [];
-	protected $attr_indexs = ["overall", "attack", "defence", "strength", "constitution", "ranged", "prayer", "magic", "cooking", "woodcutting", "fletching", "fishing", "firemaking", "crafting", "smithing", "mining", "herblore", "agility", "thieving", "slayer", "farming", "runecrafting", "hunter", "construction", "summoning", "dungeoneering", "divination",
-	"bounty hunters", "bounty hunters rogues", "dominion tower", "the crucible", "castle wars", "ba attackers", "ba defenders", "ba collectors", "ba healers", "duel tournament", "mobilising armies", "conquest", "fist of guthix", "resource race", "athletics", "we2 armadyl contribution", "we2 bandos contribution", "we2 armadyl pvp kills", "we2 bandos pvp kills", "heist guard level", "heist robber level", "cfp average"];
+	protected $skill_attrs = [];
+	protected $skill_indexs = ["overall", "attack", "defence", "strength", "constitution", "ranged", "prayer", "magic", "cooking", "woodcutting", "fletching", "fishing", "firemaking", "crafting", "smithing", "mining", "herblore", "agility", "thieving", "slayer", "farming", "runecrafting", "hunter", "construction", "summoning", "dungeoneering", "divination"];
+	protected $minigame_attrs = [];
+	protected $minigame_indexs = ["bounty hunters", "bounty hunters rogues", "dominion tower", "the crucible", "castle wars", "ba attackers", "ba defenders", "ba collectors", "ba healers", "duel tournament", "mobilising armies", "conquest", "fist of guthix", "resource race", "athletics", "we2 armadyl contribution", "we2 bandos contribution", "we2 armadyl pvp kills", "we2 bandos pvp kills", "heist guard level", "heist robber level", "cfp average"];
 
 	public function __construct($csv)
 	{
@@ -20,9 +21,19 @@ class Player implements PlayerInterface
 	{
 		$value = str_replace("_", " ", $value);
 
-		if (isset($this->attrs[$value])) return $this->attrs[$value];
+		if (isset($this->skill_attrs[$value])) return $this->skill_attrs[$value];
+		if (isset($this->minigame_attrs[$value])) return $this->minigame_attrs[$value];
 		throw new OutOfRangeException("Property doesn't exist", 1);
+	}
 
+	public function get_all_skills()
+	{
+		return $skill_attrs;
+	}
+
+	public function get_all_minigames()
+	{
+		return $minigame_attrs;
 	}
 
 	protected function parse_csv($csv)
@@ -31,10 +42,13 @@ class Player implements PlayerInterface
 
 		foreach ($highscore_rows as $index => $value) {
 			if (empty($value)) continue;
-
 			$split_row = explode( ',', $value);
 
-			$this->attrs[$this->attr_indexs[$index]] = new PlayerValue($split_row);
+			if (count($this->skill_attrs) < $index + 1) {
+				$this->minigame_attrs[$this->minigame_indexs[$index]] = new PlayerValue($split_row);
+			} else {
+				$this->skill_attrs[$this->skill_indexs[$index]] = new PlayerValue($split_row);
+			}
 		}
 	}
 
