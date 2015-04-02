ca90d2c0e0b5174e9d6a7f4a02845789ab3b1065
Christopher Rolfe <christopher.rolfe198@gmail.com>
2015-04-02 23:14:21 +0100

    Adding the two new highscores labels

--- lib/Highscores/Player.php
+++ lib/Highscores/Player.php
@@ -10,7 +10,7 @@ class Player implements PlayerInterface
 	protected $skill_attrs = [];
 	protected $skill_indexs = ["overall", "attack", "defence", "strength", "constitution", "ranged", "prayer", "magic", "cooking", "woodcutting", "fletching", "fishing", "firemaking", "crafting", "smithing", "mining", "herblore", "agility", "thieving", "slayer", "farming", "runecrafting", "hunter", "construction", "summoning", "dungeoneering", "divination"];
 	protected $minigame_attrs = [];
-	protected $minigame_indexs = ["bounty hunters", "bounty hunters rogues", "dominion tower", "the crucible", "castle wars", "ba attackers", "ba defenders", "ba collectors", "ba healers", "duel tournament", "mobilising armies", "conquest", "fist of guthix", "resource race", "athletics", "we2 armadyl contribution", "we2 bandos contribution", "we2 armadyl pvp kills", "we2 bandos pvp kills", "heist guard level", "heist robber level", "cfp average"];
+	protected $minigame_indexs = ["bounty hunters", "bounty hunters rogues", "dominion tower", "the crucible", "castle wars", "ba attackers", "ba defenders", "ba collectors", "ba healers", "duel tournament", "mobilising armies", "conquest", "fist of guthix", "resource race", "athletics", "we2 armadyl contribution", "we2 bandos contribution", "we2 armadyl pvp kills", "we2 bandos pvp kills", "heist guard level", "heist robber level", "cfp average", "cow tipping", "unknown"];
 
 	public function __construct($csv)
 	{
